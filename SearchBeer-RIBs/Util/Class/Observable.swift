fa853b98bd5f09cba669d16ac3416f982037e6c0
Mindohyeon <min50875@naver.com>
2022-10-31 12:09:49 +0900

    :wrench: Add Class - Observavle

---
+++ SearchBeer-RIBs/Util/Class/Observable.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+final class Observable<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+    
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T) {
+        value = v
+    }
+}
